7b6493f4fabb1777be84d410f370593aabd280ac
止语 <1072411694@qq.com>
2023-06-11 11:03:08 +0800

    更新 index.js

--- apps/index.js
+++ apps/index.js
@@ -275,7 +275,7 @@ export async function getcommand(e = {}, mode, msg) {
                         }
                         console.log(`兑换码可使用次数为0，已删除文件：${file} `)
                       })
-                      e.group.recallMsg(e.seq)
+                      e.group.recallMsg(e.message_id)
                       e.reply([segment.at(e.user_id), "兑换成功"])
                       return
                     }
@@ -287,7 +287,7 @@ export async function getcommand(e = {}, mode, msg) {
                       cfg.uid[uid] = Number(1)
                       cfg.used += 1
                       fs.writeFileSync(file, Yaml.stringify(cfg), 'utf8')
-                      e.group.recallMsg(e.seq)
+                      e.group.recallMsg(e.message_id)
                       e.reply([segment.at(e.user_id), "兑换成功"])
                       return
                     }
@@ -295,13 +295,13 @@ export async function getcommand(e = {}, mode, msg) {
                       cfg.used += 1
                       cfg.uid[uid] = Number(cfg.uid[uid]) + 1
                       fs.writeFileSync(file, Yaml.stringify(cfg), 'utf8')
-                      e.group.recallMsg(e.seq)
+                      e.group.recallMsg(e.message_id)
                       e.reply([segment.at(e.user_id), "兑换成功"])
                       return
                     }
                   }
                   else {
-                    e.group.recallMsg(e.seq)
+                    e.group.recallMsg(e.message_id)
                     e.reply([segment.at(e.user_id), `\n兑换失败\n${fail.join('\n')}`])
                     return
                   }
@@ -330,7 +330,7 @@ export async function getcommand(e = {}, mode, msg) {
     .catch(error => {
       console.error(error)
       if (mode === "cdk") {
-        e.group.recallMsg(e.seq)
+        e.group.recallMsg(e.message_id)
       }
       e.reply([segment.at(e.user_id), `\nUID：${uid}\n走开，你都不在线 ￣へ￣`])
     })
@@ -497,7 +497,7 @@ export async function getmail(e = {}, mode, item) {
                       }
                       console.log(`兑换码可使用次数为0，已删除文件：${file} `)
                     })
-                    e.group.recallMsg(e.seq)
+                    e.group.recallMsg(e.message_id)
                     e.reply([segment.at(e.user_id), "兑换成功"])
                     return
                   }
@@ -510,7 +510,7 @@ export async function getmail(e = {}, mode, item) {
                     cfg.uid[uid] = Number(1)
                     cfg.used += 1
                     fs.writeFileSync(file, Yaml.stringify(cfg), 'utf8')
-                    e.group.recallMsg(e.seq)
+                    e.group.recallMsg(e.message_id)
                     e.reply([segment.at(e.user_id), "兑换成功"])
                     return
                   }
@@ -520,7 +520,7 @@ export async function getmail(e = {}, mode, item) {
                     cfg.used += 1
                     cfg.uid[uid] = Number(cfg.uid[uid]) + 1
                     fs.writeFileSync(file, Yaml.stringify(cfg), 'utf8')
-                    e.group.recallMsg(e.seq)
+                    e.group.recallMsg(e.message_id)
                     e.reply([segment.at(e.user_id), "兑换成功"])
                     return
                   }
@@ -549,13 +549,13 @@ export async function getmail(e = {}, mode, item) {
               }
               else if (retcode === 1312) {
                 if (mode === "cdk") {
-                  e.group.recallMsg(e.seq)
+                  e.group.recallMsg(e.message_id)
                 }
                 e.reply([segment.at(e.user_id), `失败 -> 游戏货币超限`])
               }
               else if (retcode === 1316) {
                 if (mode === "cdk") {
-                  e.group.recallMsg(e.seq)
+                  e.group.recallMsg(e.message_id)
                 }
                 e.reply([segment.at(e.user_id), `失败 -> 游戏货币超限`])
               }
